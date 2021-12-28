a97c15235a27cad3e5bb918e68b8f31ede562f9d
jmk <whalsrnraos@naver.com>
2021-12-28 17:07:31 +0900

    자바 서블릿 연습1일차

---
+++ src/main/java/ch3/MyServlet1.java
@@ -0,0 +1,26 @@
+package ch3;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+
+@WebServlet(urlPatterns = "/ch3/myServlet1")
+public class MyServlet1 extends HttpServlet {
+
+	@Override
+    public void service(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException{
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>MyServlet1</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Fighting Korea!!!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
---
+++ src/main/webapp/ch6/directive1.jsp
@@ -0,0 +1,5 @@
+<%@page info="Copyright 2021 by Metanet" language="java"
+	contentType="text/html;charset=UTF-8"
+%>
+<h1>Directive Example1</h1>
+<%=this.getServletInfo()%>
